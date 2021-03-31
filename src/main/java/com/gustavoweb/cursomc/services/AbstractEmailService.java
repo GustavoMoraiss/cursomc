6efda4d4700981dc71751db16da09be900c6a5ff
gustavomoraiss <gustavo.morais1098@gmail.com>
2021-03-30 23:45:26 -0300

    Esqueci a senha

--- src/main/java/com/gustavoweb/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/gustavoweb/cursomc/services/AbstractEmailService.java
@@ -13,19 +13,20 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.gustavoweb.cursomc.domain.Cliente;
 import com.gustavoweb.cursomc.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService {
-	
+
 	@Value("${default.sender}")
 	private String sender;
-	
+
 	@Autowired
 	private TemplateEngine templateEngine;
-	
+
 	@Autowired
 	private JavaMailSender javaMailSender;
-	
+
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj) {
 		SimpleMailMessage sm = prepareSimpleMailMessageFromPedido(obj);
@@ -40,15 +41,15 @@ public abstract class AbstractEmailService implements EmailService {
 		sm.setSentDate(new Date(System.currentTimeMillis()));
 		sm.setText(obj.toString());
 		return sm;
-	}	
-	
+	}
+
 	protected String htmlFromTemplatePedido(Pedido obj) {
-		
+
 		Context context = new Context();
 		context.setVariable("pedido", obj);
 		return templateEngine.process("email/confirmacaoPedido", context);
 	}
-	
+
 	@Override
 	public void sendOrderConfirmationHtmlEmail(Pedido obj) {
 		MimeMessage mm;
@@ -70,8 +71,21 @@ public abstract class AbstractEmailService implements EmailService {
 		mmh.setText(htmlFromTemplatePedido(obj), true);
 		return mimeMessage;
 	}
-	
-	
-	
-	
+
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+		sendEmail(sm);
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage mmh = new SimpleMailMessage();
+		mmh.setTo(cliente.getEmail());
+		mmh.setFrom(sender);
+		mmh.setSubject("Solicitação de nova senha: ");
+		mmh.setSentDate(new Date(System.currentTimeMillis()));
+		mmh.setText("Nova senha: " + newPass);
+		return mmh;
+	}
+
 }
